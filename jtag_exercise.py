4133000389df4a5fcd931bfa1663a49efdcd502f
Rock Boynton <boyntonrl@msoe.edu>
2021-01-03 23:52:54 -0600

    Have actual exersize in a script

---
+++ jtag_exercise.py
@@ -0,0 +1,30 @@
+from ftd2jtag.ftd2jtag import bsdl2json, setup_device
+from ftd2jtag.idcode import get_real_idcode, get_idcode_opcode, verify_idcode
+from ftd2jtag.extest import get_led_boundary_idx, blink_leds
+
+# Serial number of the FTDI C232HM-DDHSL-0 USB dongle
+FTDI_CABLE = b"FTXQNTSO"
+
+
+bsdl_as_json = bsdl2json("tests/bsdl/xc2c64a_vq44.bsd")
+
+real_idcode = get_real_idcode(bsdl_as_json)
+
+idcode_opcode = get_idcode_opcode(bsdl_as_json)
+
+led_d1, led_d2 = get_led_boundary_idx(bsdl_as_json, 39, 38)
+
+device = setup_device(FTDI_CABLE)
+
+print('Verifying IDCODE...')
+if verify_idcode(device, real_idcode, idcode_opcode):
+    print("\tIDCODE read matches real IDCODE from BSDL file! ")
+else:
+    exit(-1)
+
+print('Blinking LEDs...')
+blink_leds(device, led_d1, led_d2, cycles=3, frequency=0.5)
+
+print('Done!')
+
+device.close()
