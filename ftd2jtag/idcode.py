940382b8c800574b4f49b2fc4d0739de5ae2e86d
Rock Boynton <boyntonrl@msoe.edu>
2021-01-05 10:32:07 -0600

    Comment idcode.py

--- ftd2jtag/idcode.py
+++ ftd2jtag/idcode.py
@@ -1,7 +1,20 @@
+"""IDCODE JTAG support for ftd2jtag"""
+
 from .mpsse_commands import *
 
+
 def verify_idcode(device, idcode, idcode_opcode):
-    idcode_read = read_idcode_manual(device, idcode_opcode)
+    """Verifies the read IDCODE matches the actual idcode
+
+    Args:
+        device (FTD2XX): FTDI device
+        idcode (str): actual IDCODE
+        idcode_opcode (str): opcode instruction to shift in to the chip's JTAG TAP
+
+    Returns:
+        bool: True if IDCODE matches else False
+    """
+    idcode_read = read_idcode_opcode(device, idcode_opcode)
     for i in range(len(idcode)):
         if idcode[i] == "X":
             continue  # ignore 'don't cares'
@@ -14,7 +27,14 @@ def verify_idcode(device, idcode, idcode_opcode):
 
 
 def read_idcode(device):
-    """Reads IDCODE coming out of reset"""
+    """Reads IDCODE coming out of reset
+
+    Args:
+        device (FTD2XX): FTDI device
+
+    Returns:
+        str: IDCODE read from chip
+    """
     data = bytearray()
     data.extend((WRITE_BITS_TMS_NVE, 4, 0b11111))  # go to reset
     data.extend((WRITE_BITS_TMS_NVE, 3, 0b0010))  # go to shift-dr
@@ -23,8 +43,18 @@ def read_idcode(device):
     return device.read(4)[::-1].hex("_")  # return IDCODE
 
 
-def read_idcode_manual(device, idcode_opcode):
-    """Reads IDCODE using the opcode"""
+def read_idcode_opcode(device, idcode_opcode):
+    """Reads IDCODE using the opcode
+
+    See XILINX Programmer Qualification Specification Table 38
+
+    Args:
+        device (FTD2XX): FTDI device
+        idcode_opcode (str): opcode instruction to shift in to the chip's JTAG TAP
+
+    Returns:
+        str: IDCODE read from chip
+    """
     data = bytearray()
     data.extend((WRITE_BITS_TMS_NVE, 4, 0b11111))  # go to reset
     data.extend((WRITE_BITS_TMS_NVE, 4, 0b00110))  # go to shift-ir
@@ -37,6 +67,14 @@ def read_idcode_manual(device, idcode_opcode):
 
 
 def get_idcode_opcode(bsdl_as_json):
+    """Gets the IDCODE opcode using the BSDL file for the DUT
+
+    Args:
+        bsdl_as_json (dict): BSDL file parsed into JSON
+
+    Returns:
+        str: IDCODE opcode
+    """
     instruction_registers = bsdl_as_json["instruction_register_description"]["instruction_opcodes"]
     idcode_instruction = next(
         reg for reg in instruction_registers if reg["instruction_name"] == "IDCODE"
@@ -46,6 +84,14 @@ def get_idcode_opcode(bsdl_as_json):
 
 
 def get_real_idcode(bsdl_as_json):
+    """Gets the real IDCODE using the BSDL file of the DUT
+
+    Args:
+        bsdl_as_json (dict): BSDL file parsed into JSON
+
+    Returns:
+        str: real IDCODE
+    """
     optional_registers = bsdl_as_json["optional_register_description"]
     idcode_attribute = next(reg for reg in optional_registers if reg["idcode_register"])
     idcode = idcode_attribute["idcode_register"]
